45474706f50c35782aac5811f84af66ea4fafcc8
Alexander Beley <beleysasha@gmail.com>
2023-09-21 16:27:09 +1200

    new token

--- settings/bot_settings.py
+++ settings/bot_settings.py
@@ -1,8 +1,11 @@
 from aiogram.types import LabeledPrice
 
-BOT_TOKEN = '6457448999:AAG5J8DR9UV64Ic8OTJP4hMr--u3gWsPYQo'
-API_HASH = '585569e191fe7316eb2dd02c1f1430cc'
-API_ID = 18113229
+# BOT_TOKEN = '6457448999:AAG5J8DR9UV64Ic8OTJP4hMr--u3gWsPYQo'
+BOT_TOKEN = '6661047313:AAEhQD3jN5SM-HoeF-K1VySYDtwoLdNqdb0'
+#API_HASH = '585569e191fe7316eb2dd02c1f1430cc'
+API_HASH = '085204a39f69bed8435aeccc6b0a7001'
+# API_ID = 18113229
+API_ID = 27543954
 SESSION_NAME = 'My_session'
 QUERY = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 LIMIT_USER = 200
